0f9387ed157e19b3470df0018348f50cf1e8d355
Md belayet <mdbelayethossain016@gmail.com>
2024-04-25 09:56:01 +0600

    keys background color set complete

--- scripts/alpha-clash.js
+++ scripts/alpha-clash.js
@@ -1,1 +1,33 @@
-console.log('this is js file')
+//function for play now btn in home section  
+// function play(){
+//     const homeSection = document.getElementById('home-section');
+//     homeSection.classList.add('hidden')
+//     const playGroundSection = document.getElementById('play-ground');
+//     playGroundSection.classList.remove('hidden')
+// }
+
+//function for continue game
+function continueGame(){
+    // create or generation a random alphabet
+   const alphabet = getARandomAlphabet();
+   const setAlphabet = document.getElementById('Random-alphabet-place');
+   setAlphabet.innerText = alphabet;
+
+    //set background color 
+    setBGColorById(alphabet);    
+}
+
+//function for play now btn in home section
+function play(){
+    hiddenElementById('home-section');
+    showElementById('play-ground');
+    continueGame();
+}
+
+//function for score btn in play ground section
+// function score(){
+//     const playGroundSection =document.getElementById('play-ground');
+//     playGroundSection.classList.add('hidden');
+//     const scoreSection = document.getElementById('score-section');
+//     scoreSection.classList.remove('hidden');
+// }
---
+++ scripts/utility.js
@@ -0,0 +1,39 @@
+// function for hide 
+function hiddenElementById(elementId){
+    const element = document.getElementById(elementId);
+    element.classList.add('hidden');
+}
+
+//function for show
+function showElementById(elementId){
+    const element = document.getElementById(elementId);
+    element.classList.remove('hidden');
+}
+
+// function for set background color in key board keys 
+function setBGColorById(elementId){
+    const element = document.getElementById(elementId);
+    element.classList.add('bg-[#FFA500]')
+} 
+
+// function for remove background color in key board keys 
+function removeBGColorById(elementId){
+    const element = document.getElementById(elementId);
+    element.classList.remove('bg-[#FFA500]')
+} 
+
+// function for generation a random alphabet
+function getARandomAlphabet(){
+    //get or create alphabets array
+    const alphabetString = 'abcdefghijklmnopqrstuvwxyz'
+    const alphabets = alphabetString.split('');
+
+    //get a random index between 0-25
+    const randomNumber = Math.random()*25;
+    const index = Math.round(randomNumber);
+
+    const randomAlphabet = alphabets[index];
+    return randomAlphabet;
+
+
+}
